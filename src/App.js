3273ec46d90df428d12b639b190865f7c260abbe
Christopher Williams <cw@chriswillia.ms>
2019-05-31 10:09:05 -0500

    Refactors App as a class and adds state.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import {Route} from 'react-router-dom';
 import Header from './header/header';
 import Sidebar from './sidebar/sidebar';
 import Main from './main/main';
+import DATA from './dummy-store';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main className='app'>
-        <Sidebar />
-        <Main />
-      </main>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      folders: [],
+      notes: [],
+    };
+  }
+
+  componentDidMount() {
+    // Update state with the dummy folder and note data
+    this.setState({
+      folders: DATA.folders,
+      notes: DATA.notes,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <main className='app'>
+          <Sidebar />
+          <Main />
+        </main>
+      </>
+    );
+  }
 }
+
 export default App;
